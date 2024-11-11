a73a9b743c79a744f919c42e1678fb27a498c417
ericpham <phamwangthang@gmail.com>
2024-11-11 10:36:02 +0100

    Java Projects

--- IntelliJProjects/Polozka/src/main/java/cz/itnetwork/Boty.java
+++ IntelliJProjects/Polozka/src/main/java/cz/itnetwork/Boty.java
@@ -1,4 +1,19 @@
 package cz.itnetwork;
 
-public class Boty {
+public class Boty implements IPolozka{
+    private String Nazev;
+    private int Velikost;
+
+    public Boty(String Nazev, int Velikost) {
+        this.Nazev = Nazev;
+        this.Velikost = Velikost;
+    }
+
+    public String ziskejNazev() {
+        return Nazev;
+    }
+
+    public int ziskejVelkost() {
+        return Velikost;
+    }
 }
