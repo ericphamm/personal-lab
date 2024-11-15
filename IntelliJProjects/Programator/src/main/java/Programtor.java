788481432d2bb0b655b7e32827a6aae84838c652
ericpham <phamwangthang@gmail.com>
2024-11-15 23:17:19 +0100

    Project Programator

--- IntelliJProjects/Programator/src/main/java/Programtor.java
+++ IntelliJProjects/Programator/src/main/java/Programtor.java
@@ -1,2 +1,22 @@
+import java.util.HashSet;
+
 public class Programtor {
+    private String Jmeno;
+    private HashSet<String> ProgramovaciJazyky = new HashSet<String>(); //Vytvoření kolekce Programovací jazky pomocí HashSet
+
+    public Programator(String Jmeno, String[] ProgramovaciJazyky){
+        this.Jmeno = Jmeno;
+
+        /*
+            Zápis jednotlivých programovacích jazyků z pole ProgramovaciJazyky do kolekce HashSet<String> ProgramovaciJazyky
+        */
+        for (String jazyk : ProgramovaciJazyky) {
+            this.ProgramovaciJazyky.add(jazyk);
+        }
+    }
+
+    public String getJmeno() { return this.Jmeno; }
+    public HashSet<String> getProgramovaciJazyky() { return this.ProgramovaciJazyky; }
+
 }
+
