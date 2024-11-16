7ffe1e6beb8ef28886260a8e929ac72bacd01bbd
ericpham <phamwangthang@gmail.com>
2024-11-16 14:13:03 +0100

    Kalkulacka testovanni

--- IntelliJProjects/Kalulacka/src/main/java/Kalkulacka/Kalkulacka.java
+++ IntelliJProjects/Kalulacka/src/main/java/Kalkulacka/Kalkulacka.java
@@ -1,8 +1,14 @@
 package Kalkulacka;
 
+
 public class Kalkulacka {
 
-
+    /**
+     * metoda pro scitani
+     * @param cislo1 - prvni cislo
+     * @param cislo2 - druhe cislo
+     * @return vysledek secteni
+     */
     public int scitani(int cislo1, int cislo2) {
         return cislo1 + cislo2;
     }
--- IntelliJProjects/Kalulacka/src/test/java/Kalkulacka/KalkulackaTest.java
+++ IntelliJProjects/Kalulacka/src/test/java/Kalkulacka/KalkulackaTest.java
@@ -7,13 +7,30 @@ import static org.junit.Assert.assertEquals;
 
 public class KalkulackaTest {
 
+
     @Test
     public void scitani() {
 
         Kalkulacka k = new Kalkulacka();
 
         assertEquals(2, k.scitani(1,1));
-        assertEquals(4, k.nasobeni(2,2));
-        assertEquals(3,k.odecitani(4,1));
     }
+
+    @Test
+    public void testNasobeni() {
+        Kalkulacka k = new Kalkulacka();
+
+        assertEquals(4,k.nasobeni(2,2));
+    }
+
+    @Test
+    public void testOdecitani() {
+
+        Kalkulacka k = new Kalkulacka();
+
+        assertEquals(6,k.odecitani(10,4));
+    }
+
+
+
 }
