eaf324dc7bf65fa5aa7078a1a81d209ac78d2055
ericpham <phamwangthang@gmail.com>
2024-11-22 03:27:37 +0100

    Osoby StreamAPI Done

--- IntelliJProjects/Osoby/src/main/java/Main.java
+++ IntelliJProjects/Osoby/src/main/java/Main.java
@@ -32,6 +32,12 @@ public class Main {
         System.out.println("Osoby hovořící rusky:\n " + groupPeopleByLanguage(osoby, "rusky") + ",");
         System.out.println("Osoby hovořící španělsky:\n " + groupPeopleByLanguage(osoby, "španělsky") + ",");
 
+        System.out.println("Osoby hovořící česky a zárověň německy:\n " + groupPeopleBy2Language(osoby, "česky", "německy") + ",");
+        System.out.println("Osoby nehovořící anglicky:\n " + groupPeopleNotSpeak(osoby, "anglicky") + ",");
+        System.out.println("Osoby nehovořící česky ani anglicky:\n " + groupPeopleNotSpeakAni(osoby, "česky", "anglicky") + ",");
+        System.out.println("Osoby hovořící rusky nebo španělsky:\n " + groupPeopleSpeakJedenNebo(osoby, "rusky", "španělsky") + ",");
+
+
 
 
 
@@ -40,7 +46,34 @@ public class Main {
     private static String groupPeopleByLanguage(Collection<Osoba> osoby, String language) {
         return osoby.stream()
                 .filter(osoba -> osoba.getJazyky().contains(language))
-                .map(Osoba::getJmeno) // ziskat jmeno
+                .map(Osoba::getJmeno)
+                .collect(Collectors.joining(", ")); // join osoby s carkami
+    }
+
+    private static String groupPeopleBy2Language(Collection<Osoba> osoby, String language1, String language2) {
+        return osoby.stream()
+                .filter(osoba -> osoba.getJazyky().contains(language1) && osoba.getJazyky().contains(language2))
+                .map(Osoba::getJmeno)
+                .collect(Collectors.joining(", ")); // join osoby s carkami
+    }
+
+    private static String groupPeopleNotSpeak(Collection<Osoba> osoby, String language) {
+        return osoby.stream()
+                .filter(osoba -> !osoba.getJazyky().contains(language))
+                .map(Osoba::getJmeno)
+                .collect(Collectors.joining(", ")); // join osoby s carkami
+    }
+
+    private static String groupPeopleNotSpeakAni(Collection<Osoba> osoby, String language1, String language2) {
+        return osoby.stream()
+                .filter(osoba -> !osoba.getJazyky().contains(language1) && !osoba.getJazyky().contains(language2))
+                .map(Osoba::getJmeno)
+                .collect(Collectors.joining(", ")); // join osoby s carkami
+    }
+    private static String groupPeopleSpeakJedenNebo(Collection<Osoba> osoby, String language1, String language2) {
+        return osoby.stream()
+                .filter(osoba -> osoba.getJazyky().contains(language1) || osoba.getJazyky().contains(language2))
+                .map(Osoba::getJmeno)
                 .collect(Collectors.joining(", ")); // join osoby s carkami
     }
 }
