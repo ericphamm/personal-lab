2d8c0112a889c098d76ca43f5d6f61c01f31fc33
ericpham <phamwangthang@gmail.com>
2024-12-07 20:27:42 +0100

    Javascript projects

---
+++ Javascript/pekarstvi/js/dom.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+	// let elements = document.getElementsByClassName("important");
+	// for (let i = 0; i < elements.length; i++) {
+	// 	console.log(elements[i]);
+	// }
+
+	// let paragraphs = document.getElementsByTagName("p");
+	// for (let i = 0; i < paragraphs.length; i++) {
+	// 	console.log(paragraphs[i]);
+	// }
+
+	let title = document.querySelector("#greeting");
+	let text = document.querySelector(".important");
+	let picture = document.querySelector('[src="img/pastry.jpg"]');
+	let photo = document.querySelector("logo");
+	let spans = document.querySelectorAll("span");
+
+	console.log(title);
+	console.log(text);
+	console.log(picture);
+	console.log(photo);
+
+	for (let i = 0; i < spans.length; i++) {
+		console.log(spans[i]);
+	}
+});
