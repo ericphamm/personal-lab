5d0d8addaa0788cb0a439b9270f5e323f94c0cff
ericpham <phamwangthang@gmail.com>
2024-12-12 04:22:01 +0100

    Interaktivni Tabulka

--- Javascript/interaktivniTabulka/editor-tabulek.js
+++ Javascript/interaktivniTabulka/editor-tabulek.js
@@ -22,6 +22,9 @@ function vytvorBunku() {
 	let tdInput = document.createElement("input");
 	tdInput.type = "text";
 	tdInput.onfocus = function() {
+		if (aktivniBunka) {
+			aktivniBunka.style.background = "";
+		}
 		aktivniBunka = this;
 		aktivniBunka.style.background = "yellow";
 	};
@@ -37,12 +40,52 @@ function vytvorTlacitko(popisek) {
 }
 
 function vytvorOvladaciTlacitka() {
-	vytvorTlacitko("Přidat řádek dolů");
-	vytvorTlacitko("Přidat řádek nahoru");
+	vytvorTlacitko("Přidat řádek dolů").onclick = pridejRadekDolu;
+	vytvorTlacitko("Přidat řádek nahoru").onclick = pridejRadekNahoru;
 	vytvorTlacitko("Přidat sloupec vlevo");
 	vytvorTlacitko("Přidat sloupec vpravo");
 	vytvorTlacitko("Odstranit řádek");
 	vytvorTlacitko("Odstranit sloupec");
+}
+
+function vytvorRadek() {
+	let novyRadek = document.createElement("tr");
+	let pocetBunekVPrvnimRadku = tabulka.firstElementChild.querySelectorAll("td").length;
+
+	for (let i = 0; i < pocetBunekVPrvnimRadku; i ++) {
+		novyRadek.appendChild(vytvorBunku());
+	}
+	return novyRadek;
+}
+
+function indexRadkuAktivniBunky() {
+	let uzlyTabulky = tabulka.childNodes;
+	let hledanyRadek = aktivniBunka.parentElement.parentElement;
+
+	return Array.prototype.indexOf.call(uzlyTabulky, hledanyRadek);
+}
+
+function indexSloupecAktivniBunky() {
+	let uzlyRadku = aktivniBunka.parentElement.parentElement.childNodes;
+	let hledanaBunka = aktivniBunka.parentElement;
+
+	return Array.prototype.indexOf.call(uzlyRadku, hledanaBunka);
+}
+
+function pridejRadekNahoru() {
+	let novyRadek = vytvorRadek();
+	let indexVybraneho = indexRadkuAktivniBunky();
+	tabulka.insertBefore(novyRadek, tabulka.childNodes[indexVybraneho]);
+}
+
+function pridejRadekDolu() {
+	let novyRadek = vytvorRadek();
+	let indexVybraneho = indexRadkuAktivniBunky();
+	if (tabulka.lastChild == tabulka.childNodes[indexVybraneho]) {
+		tabulka.appendChild(novyRadek);
+	} else {
+		tabulka.insertBefore(novyRadek, tabulka.childNodes[indexVybraneho + 1]);
+	}
 }
 
 window.onload = function () {
