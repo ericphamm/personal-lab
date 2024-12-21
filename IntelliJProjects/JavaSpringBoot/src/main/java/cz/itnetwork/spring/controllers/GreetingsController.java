5a99ad17cf7ffbb104a34fea48b4d1661420f507
ericpham <phamwangthang@gmail.com>
2024-12-21 05:07:09 +0100

    springboot projects

--- IntelliJProjects/JavaSpringBoot/src/main/java/cz/itnetwork/spring/controllers/GreetingsController.java
+++ IntelliJProjects/JavaSpringBoot/src/main/java/cz/itnetwork/spring/controllers/GreetingsController.java
@@ -1,4 +1,26 @@
 package cz.itnetwork.spring.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/greetings")
 public class GreetingsController {
+
+    @GetMapping("/hello")
+    public @ResponseBody String sayHi() { //pokud vracime pouze text tak to bude Responbody
+        return "Hello World!";
+    }
+
+    @GetMapping("")
+    public String sayHello() {
+        return "hello-world"; //tady vracime nazev html souboru
+    }
+
+    @GetMapping("/button")
+    public String klikni() {
+        return "tlacitko";
+    }
 }
