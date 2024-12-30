b0fe03b8d6534ad5c4c13cbc971f0dfd893aaaf6
ericpham <phamwangthang@gmail.com>
2024-12-30 03:57:37 +0100

    springboot kalkulacka

--- IntelliJProjects/JavaSpringBootCalculator/src/main/java/cz/itnetwork/spring/controllers/CalculatorController.java
+++ IntelliJProjects/JavaSpringBootCalculator/src/main/java/cz/itnetwork/spring/controllers/CalculatorController.java
@@ -1,6 +1,8 @@
 package cz.itnetwork.spring.controllers;
 
+import cz.itnetwork.spring.dto.TitleDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("calculator")
 public class CalculatorController {
 
-    @GetMapping("")
-    public String renderCalculator() {
+//    @GetMapping("")
+//    public String renderCalculator() {
+//        return "index";
+//    }
+
+    @GetMapping
+    public String renderSomething(Model model) {
+        model.addAttribute("pageTitle", "Spring Calculator!");
+
         return "index";
+    }
+
+    @GetMapping
+    public String renderCalculator(TitleDTO titleDTO) {
+        //set data intoDTO
+        titleDTO.setTitle("SpringBoot Calculator!");
+
+        //return the view name
+        return "index.html";
     }
 }
---
+++ IntelliJProjects/JavaSpringBootCalculator/src/main/java/cz/itnetwork/spring/dto/TitleDTO.java
@@ -0,0 +1,23 @@
+package cz.itnetwork.spring.dto;
+
+public class TitleDTO {
+    private String title;
+    private String description;
+
+    //gettery and settery
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
