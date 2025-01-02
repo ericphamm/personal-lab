63fc3058186475329745d01b86424eb57ff91c6a
ericpham <phamwangthang@gmail.com>
2025-01-02 01:16:05 +0100

    springboot kalkulacka

--- IntelliJProjects/JavaSpringBootCalculator/src/main/java/cz/itnetwork/spring/controllers/CalculatorController.java
+++ IntelliJProjects/JavaSpringBootCalculator/src/main/java/cz/itnetwork/spring/controllers/CalculatorController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -33,6 +34,12 @@ public class CalculatorController {
 
         //return the view name
         return "index";
+    }
+
+    @PostMapping
+    public String calculate(@ModelAttribute CalculatorDTO calculatorDTO,
+                            Model model) {
+        return "result";
     }
 
 
