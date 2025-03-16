acfb485745bad789a8c5e4fba79cbbd6f5e02225
ericpham <phamwangthang@gmail.com>
2025-03-17 00:33:27 +0100

    springboot

--- IntelliJProjects/JavaSpringBootCalculator/src/main/java/cz/itnetwork/spring/controllers/CalculatorController.java
+++ IntelliJProjects/JavaSpringBootCalculator/src/main/java/cz/itnetwork/spring/controllers/CalculatorController.java
@@ -2,6 +2,8 @@ package cz.itnetwork.spring.controllers;
 
 import cz.itnetwork.spring.dto.TitleDTO;
 import cz.itnetwork.spring.models.CalculatorDTO;
+import cz.itnetwork.spring.models.CalculatorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,15 +34,32 @@ public class CalculatorController {
         titleDTO.setTitle("SpringBoot Calculator!");
         titleDTO.setDescription("A simple calculator built with Spring Boot.");
 
+
+
         //return the view name
         return "index";
     }
 
+//    @PostMapping
+//    public String calculate(@ModelAttribute CalculatorDTO calculatorDTO,
+//                            Model model) {
+//        float calculationResult = calculatorService.calculate(calculatorDTO);
+//        model.addAttribute("calculationResult", calculationResult);
+//        return "result";
+//    }
+
     @PostMapping
     public String calculate(@ModelAttribute CalculatorDTO calculatorDTO,
                             Model model) {
-        return "result";
+        float calculationResult = calculatorService.calculate(calculatorDTO);
+        model.addAttribute("calculationResult", calculationResult);
+        return "index";
     }
+
+
+
+    @Autowired
+    private CalculatorService calculatorService;
 
 
 }
