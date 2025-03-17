7e965cf02c03f801f07e3c27e29730b0cd4af418
ericpham <phamwangthang@gmail.com>
2025-03-17 03:27:00 +0100

    springboot

--- IntelliJProjects/JavaSpringBoot/src/main/java/cz/itnetwork/spring/controllers/GreetingsController.java
+++ IntelliJProjects/JavaSpringBoot/src/main/java/cz/itnetwork/spring/controllers/GreetingsController.java
@@ -2,17 +2,19 @@ package cz.itnetwork.spring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/pozdrav")
 public class GreetingsController {
 
-    @GetMapping("/pozdrav")
-    public @ResponseBody String sayHi() {
-        return "Hello World!";
+    @GetMapping("/stranka")
+    public String sayHi() {
+        return "pozdrav";
     }
 
-    @GetMapping("/lorem")
+    @GetMapping("lorem")
     public @ResponseBody String sayLorem() {
         return "Lorem epsum";
     }
