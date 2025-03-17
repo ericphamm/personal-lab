6395b9b585d85f0d43175bb53273324a72b3817b
ericpham <phamwangthang@gmail.com>
2025-03-17 03:38:02 +0100

    springkalkulacka

---
+++ IntelliJProjects/SpringKalkulacka/src/main/java/eric/pham/SpringKalkulacka/SpringKalkulackaApplication.java
@@ -0,0 +1,13 @@
+package eric.pham.SpringKalkulacka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKalkulackaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKalkulackaApplication.class, args);
+	}
+
+}
---
+++ IntelliJProjects/SpringKalkulacka/src/test/java/eric/pham/SpringKalkulacka/SpringKalkulackaApplicationTests.java
@@ -0,0 +1,13 @@
+package eric.pham.SpringKalkulacka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKalkulackaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
