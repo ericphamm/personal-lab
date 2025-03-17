c43e37cd17bc6675b1173e3063645e4663a12ba6
ericpham <phamwangthang@gmail.com>
2025-03-17 02:21:02 +0100

    springboot

--- IntelliJProjects/JavaSpringBoot/src/main/java/cz/itnetwork/spring/controllers/GreetingsController.java
+++ IntelliJProjects/JavaSpringBoot/src/main/java/cz/itnetwork/spring/controllers/GreetingsController.java
@@ -1,10 +1,14 @@
 package cz.itnetwork.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GreetingsController {
-    public String sayHi() {
+
+    @GetMapping("/pozdrav")
+    public @ResponseBody String sayHi() {
         return "Hello World!";
     }
 }
