6453bce375ee675e570b97b70c93e3f67ec295b6
ericpham <phamwangthang@gmail.com>
2025-03-18 04:15:04 +0100

    springkalkulacka

---
+++ IntelliJProjects/SpringKalkulacka/src/main/java/eric/pham/SpringKalkulacka/services/CalculatorService.java
@@ -0,0 +1,23 @@
+package eric.pham.SpringKalkulacka.services;
+
+import eric.pham.SpringKalkulacka.dto.CalculatorDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public float calculate(CalculatorDTO calculator) {
+        switch (calculator.getOperation()) {
+            case "+":
+                return calculator.getFirstNumber() + calculator.getSecondNumber();
+            case "-":
+                return calculator.getFirstNumber() - calculator.getSecondNumber();
+            case "/":
+                if (calculator.getSecondNumber() == 0) {
+                    throw new IllegalArgumentException("Cant divide by 0");
+                }
+                return calculator.getFirstNumber() / calculator.getSecondNumber();
+            case "*":
+                return calculator.getFirstNumber() * calculator.getSecondNumber();
+        }
+    }
+}
