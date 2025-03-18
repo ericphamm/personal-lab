1efc9e6c79b654569b4816ca71198e906ac58007
ericpham <phamwangthang@gmail.com>
2025-03-18 14:08:33 +0100

    random number

---
+++ IntelliJProjects/RandomNumber/src/test/java/eric/pham/RandomNumber/RandomNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package eric.pham.RandomNumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomNumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
