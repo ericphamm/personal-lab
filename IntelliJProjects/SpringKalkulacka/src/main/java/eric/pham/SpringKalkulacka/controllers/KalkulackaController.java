f7eac53f5c3642c3679884faf7269e6196d1e1d8
ericpham <phamwangthang@gmail.com>
2025-03-18 03:54:59 +0100

    springkalkulacka

--- IntelliJProjects/SpringKalkulacka/src/main/java/eric/pham/SpringKalkulacka/controllers/KalkulackaController.java
+++ IntelliJProjects/SpringKalkulacka/src/main/java/eric/pham/SpringKalkulacka/controllers/KalkulackaController.java
@@ -1,10 +1,12 @@
 package eric.pham.SpringKalkulacka.controllers;
 
+import eric.pham.SpringKalkulacka.dto.CalculatorDTO;
 import eric.pham.SpringKalkulacka.dto.TitleDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,15 +14,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class KalkulackaController {
 
     @GetMapping
-    public String zobrazujKalkulacku(@ModelAttribute TitleDTO titleDTO) {
+    public String zobrazujKalkulacku(@ModelAttribute TitleDTO titleDTO,
+                                     @ModelAttribute CalculatorDTO calculatorDTO) {
         titleDTO.setTitle("Takhle se zobrazuje nadpis");
         return "index";
     }
 
-//    @GetMapping
-//    public String renderSomething(Model model) {
-//        model.addAttribute("pageTitle", "Toto je Spring Kalkulacka!");
-//
-//        return "index";
-//    }
+    @PostMapping
+    public String calculate(
+            @ModelAttribute CalculatorDTO calculatorDTO,
+            Model model
+    ) {
+        return "result";
+    }
 }
---
+++ IntelliJProjects/SpringKalkulacka/src/main/java/eric/pham/SpringKalkulacka/dto/CalculatorDTO.java
@@ -0,0 +1,31 @@
+package eric.pham.SpringKalkulacka.dto;
+
+public class CalculatorDTO {
+    private float firstNumber;
+    private float secondNumber;
+    private String operation;
+
+    public float getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(float firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public float getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(float secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+}
