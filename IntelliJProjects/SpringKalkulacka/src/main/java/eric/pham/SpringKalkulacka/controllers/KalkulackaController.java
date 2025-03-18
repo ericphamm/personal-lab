4ed60d51619aca393f51c0c9beefb2a27aae295d
ericpham <phamwangthang@gmail.com>
2025-03-18 02:49:40 +0100

    springkalkulacka

--- IntelliJProjects/SpringKalkulacka/src/main/java/eric/pham/SpringKalkulacka/controllers/KalkulackaController.java
+++ IntelliJProjects/SpringKalkulacka/src/main/java/eric/pham/SpringKalkulacka/controllers/KalkulackaController.java
@@ -1,23 +1,26 @@
 package eric.pham.SpringKalkulacka.controllers;
 
+import eric.pham.SpringKalkulacka.dto.TitleDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("kalkulacka")
 public class KalkulackaController {
 
-//    @GetMapping
-//    public String zobrazujKalkulacku() {
-//        return "index";
-//    }
-
     @GetMapping
-    public String renderSomething(Model model) {
-        model.addAttribute("pageTitle", "Toto je Spring Kalkulacka!");
-
+    public String zobrazujKalkulacku(@ModelAttribute TitleDTO titleDTO) {
+        titleDTO.setTitle("Takhle se zobrazuje nadpis");
         return "index";
     }
+
+//    @GetMapping
+//    public String renderSomething(Model model) {
+//        model.addAttribute("pageTitle", "Toto je Spring Kalkulacka!");
+//
+//        return "index";
+//    }
 }
