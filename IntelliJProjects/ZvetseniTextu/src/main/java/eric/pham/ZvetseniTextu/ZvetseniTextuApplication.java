608c9b1d47e5d2367aff2a8c20c3a697f50e4cac
ericpham <phamwangthang@gmail.com>
2025-03-18 19:48:15 +0100

    zvetseni textu

---
+++ IntelliJProjects/ZvetseniTextu/src/main/java/eric/pham/ZvetseniTextu/ZvetseniTextuApplication.java
@@ -0,0 +1,13 @@
+package eric.pham.ZvetseniTextu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZvetseniTextuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZvetseniTextuApplication.class, args);
+	}
+
+}
