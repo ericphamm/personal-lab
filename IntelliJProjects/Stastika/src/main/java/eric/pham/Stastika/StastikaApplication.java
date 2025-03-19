c4e18821317e4816b15a7a1dbdeca6a0826366bb
ericpham <phamwangthang@gmail.com>
2025-03-19 03:11:37 +0100

    Stastika

---
+++ IntelliJProjects/Stastika/src/main/java/eric/pham/Stastika/StastikaApplication.java
@@ -0,0 +1,13 @@
+package eric.pham.Stastika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StastikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StastikaApplication.class, args);
+	}
+
+}
---
+++ IntelliJProjects/Stastika/src/test/java/eric/pham/Stastika/StastikaApplicationTests.java
@@ -0,0 +1,13 @@
+package eric.pham.Stastika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StastikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
