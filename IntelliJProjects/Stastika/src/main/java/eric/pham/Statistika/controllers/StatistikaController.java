57710172f70cc85f2091d79d9f4d248bd437e0f1
ericpham <phamwangthang@gmail.com>
2025-03-20 00:41:57 +0100

    Stastika

--- IntelliJProjects/Stastika/src/main/java/eric/pham/Statistika/controllers/StatistikaController.java
+++ IntelliJProjects/Stastika/src/main/java/eric/pham/Statistika/controllers/StatistikaController.java
@@ -1,7 +1,40 @@
 package eric.pham.Statistika.controllers;
 
+import eric.pham.Statistika.dto.StatistikaDTO;
+import eric.pham.Statistika.services.StatistikaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.OptionalDouble;
 
 @Controller
 public class StatistikaController {
+
+    @Autowired
+    StatistikaService statistikaService;
+
+    @GetMapping("/statistika")
+    public String renderIndex(@ModelAttribute StatistikaDTO statistikaDTO,
+                            Model model) {
+        model.addAttribute("soucet", "");
+        return "index";
+    }
+
+    @PostMapping("/statistika")
+    public String renderStatistika(@ModelAttribute StatistikaDTO statistikaDTO,
+                                   Model model) {
+        int soucet = statistikaService.spocitejSoucet(statistikaDTO);
+        int nejmensi = statistikaService.spocitejNejmensi(statistikaDTO);
+        int nejvetsi = statistikaService.spocitejNejvetsi(statistikaDTO);
+        float prumer = statistikaService.spocitejPrumer(statistikaDTO);
+        model.addAttribute("soucet", soucet);
+        model.addAttribute("nejmensi", nejmensi);
+        model.addAttribute("nejvetsi", nejvetsi);
+        model.addAttribute("prumer", prumer);
+        return "index";
+    }
 }
--- IntelliJProjects/Stastika/src/main/java/eric/pham/Statistika/services/StatistikaService.java
+++ IntelliJProjects/Stastika/src/main/java/eric/pham/Statistika/services/StatistikaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,4 +21,18 @@ public class StatistikaService {
         return cisla.stream().mapToInt(Integer::intValue).sum();
     }
 
+    public int spocitejNejmensi (StatistikaDTO statistikaDTO) {
+        List<Integer> cisla = convertToList(statistikaDTO);
+        return cisla.stream().mapToInt(Integer::intValue).min().orElse(0);
+    }
+
+    public int spocitejNejvetsi(StatistikaDTO statistikaDTO) {
+        List<Integer> cisla = convertToList(statistikaDTO);
+        return cisla.stream().mapToInt(Integer::intValue).max().orElse(0);
+    }
+
+    public float spocitejPrumer(StatistikaDTO statistikaDTO) {
+        List<Integer> cisla = convertToList(statistikaDTO);
+        return (float) cisla.stream().mapToInt(Integer::intValue).average().orElse(0);
+    }
 }
