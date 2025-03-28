0d3c6f1dbc5423a1b7b9ed2e649f2bfa9d9019b8
ericpham <phamwangthang@gmail.com>
2025-03-28 13:21:54 +0100

    Validation Bean API in the app

--- IntelliJProjects/SpringKalkulacka/src/main/java/eric/pham/SpringKalkulacka/controllers/KalkulackaController.java
+++ IntelliJProjects/SpringKalkulacka/src/main/java/eric/pham/SpringKalkulacka/controllers/KalkulackaController.java
@@ -3,9 +3,12 @@ package eric.pham.SpringKalkulacka.controllers;
 import eric.pham.SpringKalkulacka.dto.CalculatorDTO;
 import eric.pham.SpringKalkulacka.dto.TitleDTO;
 import eric.pham.SpringKalkulacka.services.CalculatorService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -16,19 +19,29 @@ public class KalkulackaController {
     private CalculatorService calculatorService;
 
     @GetMapping("/kalkulacka")
-    public String zobrazujKalkulacku(@ModelAttribute TitleDTO titleDTO,
-                                     @ModelAttribute CalculatorDTO calculatorDTO) {
-        titleDTO.setTitle("Takhle se zobrazuje nadpis");
+    public String zobrazujKalkulacku(Model model) {
+        TitleDTO titleDTO = new TitleDTO();
+        titleDTO.setTitle("Vitejte ve SpringKalkulace!");
+        model.addAttribute("titleDTO", titleDTO);
+        model.addAttribute("calculatorDTO", new CalculatorDTO());
         return "index";
     }
 
     @PostMapping("/kalkulacka")
     public String calculate(
-            @ModelAttribute CalculatorDTO calculatorDTO,
-            Model model,
-            @ModelAttribute TitleDTO titleDTO
+            @Valid @ModelAttribute("calculatorDTO") CalculatorDTO calculatorDTO,
+            BindingResult bindingResult,
+            Model model
     ) {
-        titleDTO.setTitle("Takhle se zobrazuje nadpis");
+        // Add title to model
+        TitleDTO titleDTO = new TitleDTO();
+        titleDTO.setTitle(bindingResult.hasErrors() ? "Zadajte spravne hodnoty" : "Takhle se zobrazuje nadpis");
+        model.addAttribute("titleDTO", titleDTO);
+
+        if (bindingResult.hasErrors()) {
+            return "index";
+        }
+
         float calculationResult = calculatorService.calculate(calculatorDTO);
         model.addAttribute("calculationResult", calculationResult);
         return "index";
--- IntelliJProjects/SpringKalkulacka/src/main/java/eric/pham/SpringKalkulacka/dto/CalculatorDTO.java
+++ IntelliJProjects/SpringKalkulacka/src/main/java/eric/pham/SpringKalkulacka/dto/CalculatorDTO.java
@@ -1,23 +1,32 @@
 package eric.pham.SpringKalkulacka.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 public class CalculatorDTO {
-    private float firstNumber;
-    private float secondNumber;
+    @NotNull(message = "Zadejte prvni cislo")
+    private Float firstNumber;
+
+    @NotNull(message = "Zadejte druhe cislo")
+    private Float secondNumber;
+
+    @NotEmpty(message = "Vyberte operaci")
     private String operation;
 
-    public float getFirstNumber() {
+    public Float getFirstNumber() {
         return firstNumber;
     }
 
-    public void setFirstNumber(float firstNumber) {
+    public void setFirstNumber(Float firstNumber) {
         this.firstNumber = firstNumber;
     }
 
-    public float getSecondNumber() {
+    public Float getSecondNumber() {
         return secondNumber;
     }
 
-    public void setSecondNumber(float secondNumber) {
+    public void setSecondNumber(Float secondNumber) {
         this.secondNumber = secondNumber;
     }
 
