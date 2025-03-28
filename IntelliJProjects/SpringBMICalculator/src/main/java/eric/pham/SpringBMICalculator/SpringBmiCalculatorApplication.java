38bba59a41798cbc9a17bb4ca7b14739ec0008e7
ericpham <phamwangthang@gmail.com>
2025-03-28 14:11:36 +0100

    Spring BMI Calculator

---
+++ IntelliJProjects/SpringBMICalculator/src/main/java/eric/pham/SpringBMICalculator/SpringBmiCalculatorApplication.java
@@ -0,0 +1,13 @@
+package eric.pham.SpringBMICalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBmiCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBmiCalculatorApplication.class, args);
+	}
+
+}
---
+++ IntelliJProjects/SpringBMICalculator/src/test/java/eric/pham/SpringBMICalculator/SpringBmiCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package eric.pham.SpringBMICalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBmiCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
