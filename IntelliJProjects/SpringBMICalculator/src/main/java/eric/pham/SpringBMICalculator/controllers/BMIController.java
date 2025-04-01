841a36e752fa9d44c1ed9aeabc06c73b543e1861
ericpham <phamwangthang@gmail.com>
2025-04-01 02:34:38 +0200

    BMI Calculator again

--- IntelliJProjects/SpringBMICalculator/src/main/java/eric/pham/SpringBMICalculator/controllers/BMIController.java
+++ IntelliJProjects/SpringBMICalculator/src/main/java/eric/pham/SpringBMICalculator/controllers/BMIController.java
@@ -1,32 +1,12 @@
-package eric.pham.SpringBMICalculator.controllers;
-
-import eric.pham.SpringBMICalculator.dto.BMIDTO;
-import eric.pham.SpringBMICalculator.services.BMIService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BMIController {
 
-    @Autowired
-    BMIService bmiService;
-
     @GetMapping("/bmicalculator")
-    public String showCalculator(Model model) {
-        model.addAttribute("bmiDTO", new BMIDTO());
-        return "index";
-    }
-
-    @PostMapping("/bmicalculator")
-    public String calculate(@ModelAttribute BMIDTO bmiDTO,
-                            Model model) {
-        double bmiScore = bmiService.calculate(bmiDTO);
-        model.addAttribute("bmiScore", bmiScore);
-        model.addAttribute("bmiDTO",bmiDTO);
+    public String showForm(Model model) {
         return "index";
     }
 }
