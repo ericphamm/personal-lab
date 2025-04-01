a1ac0a758392ae1674c976482ff7762897a5a0ff
ericpham <phamwangthang@gmail.com>
2025-04-01 03:53:07 +0200

    BMI Calculator CSS added by Claude

--- IntelliJProjects/SpringBMICalculator/src/main/java/eric/pham/SpringBMICalculator/services/BMIService.java
+++ IntelliJProjects/SpringBMICalculator/src/main/java/eric/pham/SpringBMICalculator/services/BMIService.java
@@ -14,7 +14,7 @@ public class BMIService {
         if (bmi < 18.5) {
             return "Underweight";
         } else if (bmi < 25) {
-            return "Normal weight";
+            return "Normal weight!";
         } else if (bmi < 30) {
             return "Overweight";
         } else if (bmi < 35) {
