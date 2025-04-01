db8209a405cff5ae4abd8992e9450da0be9a8a78
ericpham <phamwangthang@gmail.com>
2025-04-02 00:18:59 +0200

    fragment,footer

---
+++ IntelliJProjects/SpringBlog/src/main/java/eric/pham/springblog/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package eric.pham.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String renderIndex() {
+        return "pages/home/index";
+    }
+}
