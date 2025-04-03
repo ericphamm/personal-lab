120580957dfae91c83288988e193786bef1e7dc5
ericpham <phamwangthang@gmail.com>
2025-04-03 02:07:34 +0200

    static subpages

--- IntelliJProjects/SpringBlog/src/main/java/eric/pham/springblog/controllers/HomeController.java
+++ IntelliJProjects/SpringBlog/src/main/java/eric/pham/springblog/controllers/HomeController.java
@@ -10,4 +10,19 @@ public class HomeController {
     public String renderIndex() {
         return "pages/home/index";
     }
+
+    @GetMapping("/contact")
+    public String renderContact() {
+        return "pages/home/contact";
+    }
+
+    @GetMapping("/references")
+    public String renderReferences() {
+        return "pages/home/references";
+    }
+
+    @GetMapping("/skills")
+    public String renderSkills() {
+        return "pages/home/skills";
+    }
 }
