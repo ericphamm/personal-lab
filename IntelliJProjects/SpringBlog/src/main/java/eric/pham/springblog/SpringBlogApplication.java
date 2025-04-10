180d8caed2d34f5889de18929c2ea3fefba56975
ericpham <phamwangthang@gmail.com>
2025-04-10 18:11:31 +0200

    connected to database

--- IntelliJProjects/SpringBlog/src/main/java/eric/pham/springblog/SpringBlogApplication.java
+++ IntelliJProjects/SpringBlog/src/main/java/eric/pham/springblog/SpringBlogApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories // <-- Přidáním tohoto řádku "aktivujeme" JPA v našem Spring Boot projektu
 @SpringBootApplication
+@EnableJpaRepositories // <-- Přidáním tohoto řádku "aktivujeme" JPA v našem Spring Boot projektu
 public class SpringBlogApplication {
 
     public static void main(String[] args) {
